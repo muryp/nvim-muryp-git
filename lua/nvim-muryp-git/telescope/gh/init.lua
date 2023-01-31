8252f473fdced8b5de1265a5e3c3dff7ad3bd388
alifprihantoro <alifmuryp@gmail.com>
2023-01-31 09:19:46 +0700

    refactor: delete not use and merge func

--- lua/nvim-muryp-git/telescope/gh/init.lua
+++ lua/nvim-muryp-git/telescope/gh/init.lua
@@ -39,16 +39,15 @@ M.getListIssueHistory = function()
     table.insert(LIST_ISSUE, FILE_NAME)
   end
   local callback = function(selection)
-    local GET_GIT_DIR = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), '\n', '')
-    local DIR_ISSUE = GET_GIT_DIR .. '/.git/muryp/'
+    local defindMaps = require('nvim-muryp-git.telescope.gh.ghIssue.maps').maps
     if type(selection) == 'string' then
       vim.cmd('e ' .. DIR_ISSUE .. selection)
-      require('nvim-muryp-git.telescope.gh.ghIssue.maps').maps()
+      defindMaps()
       return
     end
     for _, value in pairs(selection) do
       vim.cmd('e ' .. DIR_ISSUE .. value)
-      require('nvim-muryp-git.telescope.gh.ghIssue.maps').maps()
+      defindMaps()
     end
   end
 
