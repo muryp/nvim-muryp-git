94b29f9c6725ce6a971c9f584a4f48e2e510d97d
alifprihantoro <alifprihantoro@gmail.com>
2023-06-04 21:10:20 +0700

    refactor: git telescope

--- lua/nvim-muryp-git/git/telescope/git.lua
+++ lua/nvim-muryp-git/git/telescope/git.lua
@@ -2,40 +2,63 @@ local picker = require "nvim-muryp-git.utils.picker"
 local cmdGit = require('nvim-muryp-git.git')
 local M = {}
 
----@return nil gitSshPush git commit, pull, push with opts remote
-M.push = function()
-  local LIST_REMOTE = vim.fn.systemlist("git remote") --- @type string[]
-  local function callback(selection)
-    cmdGit.gitMainCmd({
-      add = true,
-      commit = true,
-      ssh = true,
-      remote = selection,
-      pull_quest = true,
-      push = true,
-    })
-  end
-  picker({
-    opts = LIST_REMOTE,
-    callBack = callback,
-    title = "choose remote want to push"
-  })
-end
-
 ---@return nil gitPull git pull with opts remote
-M.pull = function()
+M.listRemote = function(callback)
   local LIST_REMOTE = vim.fn.systemlist("git remote") --- @type string[]
-  local function callback(selection)
-    cmdGit.gitMainCmd({
-      ssh = true,
-      remote = selection,
-      pull = true,
-    })
-  end
   picker({
     opts = LIST_REMOTE,
     callBack = callback,
     title = "choose remote want to pull"
   })
+end
+
+---@param selection string|string[] user select
+---@param cmd function arg selection => string
+---@return nil callback exec callback with selection in first arg
+local isMultiSelect = function(selection, cmd)
+  if type(selection) == 'table' then
+    for _, v in pairs(selection) do
+      cmd(v)
+    end
+  else
+    cmd(selection)
+  end
+end
+
+---@return nil gitSshPush git commit, pull, push with opts remote
+M.push = function()
+  ---defind callback/after enter
+  ---@param selection string|string[] user select
+  local callback = function(selection)
+  ---defind cmd to exec
+  ---@param remote string user select
+    local cmd = function(remote)
+      cmdGit.gitMainCmd({
+        add = true,
+        commit = true,
+        ssh = true,
+        remote = remote,
+        pull_quest = true,
+        push = true,
+      })
+    end
+    isMultiSelect(selection, cmd)
+  end
+  M.listRemote(callback)
+end
+
+---@return nil gitPull git pull with opts remote
+M.pull = function()
+  local function callback(selection)
+    local cmd = function(remote)
+      cmdGit.gitMainCmd({
+        ssh = true,
+        remote = remote,
+        pull = true,
+      })
+    end
+    isMultiSelect(selection, cmd)
+  end
+  M.listRemote(callback)
 end
 return M
