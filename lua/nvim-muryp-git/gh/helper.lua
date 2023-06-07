dc588fdcf163057ad2a1df94cf4c679ef459e51e
alifprihantoro <alifprihantoro@gmail.com>
2023-06-07 09:21:36 +0700

    docs: update helper mapping

--- lua/nvim-muryp-git/gh/helper.lua
+++ lua/nvim-muryp-git/gh/helper.lua
@@ -1,10 +1,10 @@
 return [[
 <!--
 how use it:
-- <leader><leader>gd => DELETE
-- <leader><leader>ge => EDIT
-- <leader><leader>gp => POST
-- <leader><leader>gu => UPDATE
+- <leader>gid => DELETE
+- <leader>gie => EDIT
+- <leader>gip => POST
+- <leader>giu => UPDATE
 - enter in link to open browser (use vimwiki)
 
 NOTE : puth text under comment(all comment eddit is no effect )
